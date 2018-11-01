263c11a14bab6a784c33a280fda94029e685c0ea
laggy1234 <knsonu@gmail.com>
2018-11-01 21:14:18 +0530

    Save file update

---
+++ Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMobility : MonoBehaviour
+{
+	public float speed;
+	void FixedUpdate()
+	{
+		
+	}
+}
